d0095c782057144f9aca60f219ec9c966c194a8c
leveritable1977-sudo <leveritable1977@gmail.com>
2026-09-10 19:34:20 +0200

    Del 5: skapar en array av hundar

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Dog dog1 = new Dog("Rex", 3, "Schäfer", 25.5);
-        Dog dog2 = new Dog(Bella", 5, "Labrador", 30.0);
+        Dog dog2 = new Dog("Bella", 5, "Labrador", 30.0);
         Dog dog3 = new Dog("Milo", 2, "Pudel", 8.5);
+
+         Dog[] hundLista = {dog1, dog2, dog3};
     }
 }
