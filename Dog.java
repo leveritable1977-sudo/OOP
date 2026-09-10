9d8f0031c8ec6a5bc3155e6d087f57c5b4dcea45
leveritable1977-sudo <leveritable1977@gmail.com>
2026-09-10 10:27:22 +0200

    Del 2: konstruktor och vikt

--- Dog.java
+++ Dog.java
@@ -1,5 +1,12 @@
 public class Dog {
-    public String name;
-    public int age;
-    public String breed;
+     String name;
+     int age;
+    String breed;
+    double weight;
+    public Dog(String name, int age, String breed, double weight) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.weight = weight;
+    }
 }
