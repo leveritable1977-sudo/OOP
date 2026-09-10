28b2a6dbd29e58bc6a5aec83473abe8b6ec577f7
leveritable1977-sudo <leveritable1977@gmail.com>
2026-09-10 18:59:39 +0200

    Del4: skapar main metoden och hundobjekten

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Rex", 3, "Schäfer", 25.5);
+        Dog dog2 = new Dog(Bella", 5, "Labrador", 30.0);
+        Dog dog3 = new Dog("Milo", 2, "Pudel", 8.5);
+    }
+}
